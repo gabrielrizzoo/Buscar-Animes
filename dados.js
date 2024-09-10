0cae1a88abe6800be8c48406df13681985d4b213
Gabriel Rizzo <150742296+gabrielrizzoo@users.noreply.github.com>
2024-09-10 09:46:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Bocchi The Rock!",
+        link1 : "https://www.crunchyroll.com/pt-br/series/GXJHM3P19/bocchi-the-rock",
+        descricao : "Bocchi the Rock! é um anime que acompanha a história de Hitori 'Bocchi' Goto, uma jovem tímida e apaixonada por guitarra que sonha em tocar em uma banda. A série retrata de forma divertida e comovente os desafios que Bocchi enfrenta para superar sua timidez e se conectar com outras pessoas, enquanto explora a paixão pela música e a amizade.",
+        link : "https://pt.wikipedia.org/wiki/Bocchi_the_Rock!",
+        tags: "comédia"},
+    {
+        titulo : "One Piece",
+        link1 : "https://www.crunchyroll.com/pt-br/series/GRMG8ZQZR/one-piece",
+        descricao : "One Piece é um mangá japonês escrito e ilustrado por Eiichiro Oda. A história gira em torno de Monkey D. Luffy, um jovem que sonha em se tornar o Rei dos Piratas e encontrar o tesouro One Piece.",
+        link : "https://pt.wikipedia.org/wiki/One_Piece",
+        tags: "aventura"},
+    {
+        titulo : "Jujutsu Kaisen",
+        link1 : "https://www.crunchyroll.com/pt-br/series/GRDV0019R/jujutsu-kaisen",
+        descricao : "Jujutsu Kaisen é um mangá escrito e ilustrado por Gege Akutami. A história acompanha Yuji Itadori, um estudante do ensino médio que se envolve com o mundo da magia jujutsu após comer o dedo de um poderoso demônio.",
+        link : "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen",
+        tags: "luta"}
+];
